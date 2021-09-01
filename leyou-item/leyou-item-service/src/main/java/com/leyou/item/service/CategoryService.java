128031b3b1010054a3a965b21cf3e5b1025ac7da
kuangshen <38749742@qq.com>
2021-09-01 15:07:28 +0800

    First commit

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,28 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    CategoryMapper categoryMapper;
+
+    /**
+     *
+     * 根据父节点查询子节点
+     * @param pid
+     * @return
+     */
+    public List<Category> queryCategoriesByPid(Long pid) {
+        Category cg = new Category();
+        cg.setParentId(pid);
+        return this.categoryMapper.select(cg);
+    }
+}
