c9303361d7083a8a1eabe4bd2de66655584dab30
kuangshen <38749742@qq.com>
2022-01-28 17:43:17 +0800

    master commit2022/1/28

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/SpecificationController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/SpecificationController.java
@@ -2,6 +2,7 @@ package com.leyou.item.controller;
 
 import com.leyou.item.service.SpecificationService;
 import com.leyou.pojo.SpecGroup;
+import com.leyou.pojo.SpecParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
 import java.util.List;
@@ -19,6 +21,11 @@ public class SpecificationController {
     @Autowired
     private SpecificationService specificationService;
 
+    /**
+     * 根据cid 查询组
+     * @param cid
+     * @return
+     */
     @GetMapping("/groups/{cid}")
     public ResponseEntity<List<SpecGroup>> queryGroupsByCid(@PathVariable("cid")Long cid){
         List groups = this.specificationService.queryGroupsByCid(cid);
@@ -27,4 +34,13 @@ public class SpecificationController {
         }
         return ResponseEntity.ok(groups);
     }
+    @GetMapping("/params")
+    public ResponseEntity<List<SpecParam>> querySpecParam(@RequestParam("gid")Long gid ){
+        List params = this.specificationService.querySpecParam(gid);
+        if(CollectionUtils.isEmpty(params)){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(params);
+    }
 }
+
--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -1,7 +1,9 @@
 package com.leyou.item.service;
 
 import com.leyou.item.mapper.GroupMapper;
+import com.leyou.item.mapper.SpecParamMapper;
 import com.leyou.pojo.SpecGroup;
+import com.leyou.pojo.SpecParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,19 @@ public class SpecificationService {
     @Autowired
     private GroupMapper groupMapper;
 
+    @Autowired
+    private SpecParamMapper specParamMapper;
+
 
     public List<SpecGroup> queryGroupsByCid(Long cid) {
         SpecGroup specGroup = new SpecGroup();
         specGroup.setCid(cid);
         return groupMapper.select(specGroup);
+    }
+
+    public List<SpecParam> querySpecParam(Long gid) {
+        SpecParam record = new SpecParam();
+        record.setGroupId(gid);
+        return specParamMapper.select(record);
     }
 }
