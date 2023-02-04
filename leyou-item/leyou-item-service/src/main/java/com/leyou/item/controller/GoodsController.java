aba741a027057dcab2765577ce73bab8d2e91051
kuangshen <38749742@qq.com>
2023-02-04 11:08:13 +0800

    master commit 2023/2/4

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/GoodsController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/GoodsController.java
@@ -6,6 +6,7 @@ import com.leyou.item.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -21,11 +22,11 @@ public class GoodsController {
             @RequestParam(value = "rows",defaultValue = "5") Integer rows
 
     ){
-        PageResult<SpuBo> PageResult = this.goodsService.querySpuBoByPage(key, saleable, page, rows);
-
-
-
-        return null;
+        PageResult<SpuBo> pageResult = this.goodsService.querySpuBoByPage(key, saleable, page, rows);
+        if (CollectionUtils.isEmpty(pageResult.getItems())){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(pageResult);
     }
 
 
--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/GoodsService.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/GoodsService.java
@@ -2,11 +2,43 @@ package com.leyou.item.service;
 
 import com.leyou.PageResult;
 import com.leyou.item.bo.SpuBo;
+import com.leyou.item.mapper.BrandMapper;
+import com.leyou.item.mapper.SpuMapper;
+import com.leyou.item.pojo.Spu;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 @Service
 public class GoodsService {
+
+    @Autowired
+    private SpuMapper spuMapper;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private BrandMapper brandMapper;
+
     public PageResult<SpuBo> querySpuBoByPage(String key, Boolean saleable, Integer page, Integer rows) {
+        Example example = new Example(Spu.class);
+        Example.Criteria criteria = example.createCriteria();
+        // 搜索条件
+        if (StringUtils.isNotBlank(key)){
+            criteria.andLike()
+        }
+
+        // 分页条件
+        // 执行查询
+
+            // copy共同属性的值到新的对象
+            // 查询分类名称
+            // 查询品牌名称
+
+
+
         return null;
     }
 }
